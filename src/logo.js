0ec052f4093d85b060c5e8560f1dafec7d527d3c
Super BianzZz <89448318+dreambabyyy@users.noreply.github.com>
2025-01-10 08:46:56 +0000

    Add files via upload

---
+++ src/logo.js
@@ -0,0 +1,29 @@
+const printLogo = () => {
+  const logo = `
+╔═════════════════════════════════════════════════╗
+║                                                 ║
+██████╗ ██╗███╗   ██╗███████╗████████╗███████╗    ║
+██╔══██╗██║████╗  ██║██╔════╝╚══██╔══╝██╔════╝    ║
+██████╔╝██║██╔██╗ ██║█████╗     ██║   █████╗      ║
+██╔═══╝ ██║██║╚██╗██║██╔══╝     ██║   ██╔══╝      ║
+██║     ██║██║ ╚████║███████╗   ██║   ███████╗    ║
+╚═╝     ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚══════╝    ║
+║  SUPER BIANZZZ                                  ║
+║                                                 ║
+║                                                 ║
+║  JOIN TELEGRAM CHANNEL NOW!                     ║
+║  https://t.me/superbianz                        ║
+║  @SuperBianzZz - OFFICIAL                       ║
+║  CHANNEL                                        ║
+║                                                 ║
+║  FAST - RELIABLE - SECURE                       ║
+║                                                 ║
+║                                                 ║
+╚═════════════════════════════════════════════════╝
+  `;
+  
+  console.log(logo.yellow);
+  console.log("Super BianzZz".yellow);
+};
+
+module.exports = printLogo;
